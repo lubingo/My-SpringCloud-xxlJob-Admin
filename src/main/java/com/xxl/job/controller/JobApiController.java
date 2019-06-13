63c75de24c328c005173fb848b88811fecfd55b5
lubing <15810172080@139.com>
2019-06-13 18:12:00 +0800

    Initial commit

---
+++ src/main/java/com/xxl/job/controller/JobApiController.java
@@ -0,0 +1,34 @@
+package com.xxl.job.controller;
+
+import com.xxl.job.controller.annotation.PermissionLimit;
+import com.xxl.job.core.biz.AdminBiz;
+import com.xxl.job.core.conf.XxlJobScheduler;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xuxueli on 17/5/10.
+ */
+@Controller
+public class JobApiController implements InitializingBean {
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @RequestMapping(AdminBiz.MAPPING)
+    @PermissionLimit(limit=false)
+    public void api(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        XxlJobScheduler.invokeAdminService(request, response);
+    }
+
+
+}
